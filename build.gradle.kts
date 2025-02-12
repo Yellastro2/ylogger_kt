1a6fcb318c6f559843f114737d46dc402095441b
Yellastro2 <77885272+Yellastro2@users.noreply.github.com>
2025-02-12 14:24:09 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,3 @@ dependencies {
     implementation(kotlin("stdlib"))
 }
 
-repositories {
-    mavenCentral()  // Это основной репозиторий для Kotlin и других зависимостей
-    google()        // Если используете Android, также добавьте Google репозиторий
-}
